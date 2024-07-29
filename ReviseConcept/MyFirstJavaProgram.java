1f9c251268f4a0db21ff929e4c9d486acadc958e
anshuman <anshusinghjobportal@gmail.com>
2024-07-29 17:49:56 +0530

    FirstCommit

---
+++ ReviseConcept/MyFirstJavaProgram.java
@@ -0,0 +1,12 @@
+public class MyFirstJavaProgram {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+      for(int i=0;i<=10;i++)
+	  System.out.println("Hello Java with updated code");
+	  
+   }
+}
